2bdb021414b5e52dd09f485838478179a34038c1
victorluadev <streamyodin@gmail.com>
2022-12-11 22:03:47 -0300

    feat: Adicionando rota de busca por id

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -11,6 +11,23 @@ class PessoaController {
     }
 
   }
+
+  static async getPessoaById(req, res) {
+    const { id } = req.params;
+
+    try{
+      const pessoa = await database.Pessoas.findOne({
+        where: {
+          id: Number(id)
+        }
+      });
+
+      return res.status(200).json(pessoa);
+
+    } catch(err) {
+      return res.status(500).json({message: err.message});
+    }
+  }
 }
 
 module.exports = PessoaController;
--- src/routes/pessoasRouter.js
+++ src/routes/pessoasRouter.js
@@ -3,6 +3,8 @@ const PessoaController = require('../controllers/PessoaController.js');
 
 const router = Router();
 
-router.get('/pessoas', PessoaController.getPessoas)
+router
+  .get('/pessoas', PessoaController.getPessoas)
+  .get('/pessoas/:id', PessoaController.getPessoaById)
 
 module.exports = router;
