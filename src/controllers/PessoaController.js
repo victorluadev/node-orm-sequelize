8e488fd52bc7345f689777c13f513554a3ddacb1
victorluadev <streamyodin@gmail.com>
2022-12-11 23:08:18 -0300

    feat: Adicionando rota de DELETE

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -39,6 +39,45 @@ class PessoaController {
       return res.status(500).json({message: err.message});
     }
   }
+
+  static async update(req, res) {
+    const { id } = req.params;
+    const updatePessoa = req.body;
+
+    try{
+      await database.Pessoas.update(updatePessoa, {
+        where: {
+          id: Number(id)
+        }
+      });
+
+      const updatedPessoa = await database.Pessoas.findOne({
+        where: {
+          id: Number(id)
+        }
+      });
+
+      return res.status(200).json(updatedPessoa);
+    } catch(err) {
+      return res.status(500).json({message: err.message});
+    }
+  }
+
+  static async delete(req, res) {
+    const { id } = req.params;
+
+    try {
+      await database.Pessoas.destroy({
+        where: {
+          id: Number(id)
+        }
+      });
+
+      return res.status(200).send({message: `ID ${id} successfully deleted`});
+    } catch (err) {
+      return res.status(500).json({message: err.message});
+    }
+  }
 }
 
 module.exports = PessoaController;
--- src/routes/pessoasRouter.js
+++ src/routes/pessoasRouter.js
@@ -7,5 +7,7 @@ router
   .get('/pessoas', PessoaController.getPessoas)
   .get('/pessoas/:id', PessoaController.getPessoaById)
   .post('/pessoas', PessoaController.create)
+  .put('/pessoas/:id', PessoaController.update)
+  .delete('/pessoas/:id', PessoaController.delete)
 
 module.exports = router;
